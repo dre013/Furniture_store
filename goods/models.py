e1bb0e014028fdbab9d4b08f76635458f6eb2468
Andrey Kuznetsov <nice.andrey013@gmail.com>
2024-01-25 03:53:59 +0300

    Added model 'Category'

--- goods/models.py
+++ goods/models.py
@@ -1,3 +1,15 @@
+from enum import unique
+from tabnanny import verbose
 from django.db import models
 
-# Create your models here.
+
+class Categories(models.Model):
+    name = models.CharField(max_length=150, unique=True, verbose_name="Category")
+    slug = models.SlugField(
+        max_length=200, unique=True, blank=True, null=True, verbose_name="URL"
+    )
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+        db_table = "Category"
