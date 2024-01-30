eaf29aee5cb71cefa63e00e08bb54f99a203da97
Andrey Kuznetsov <nice.andrey013@gmail.com>
2024-01-30 21:06:20 +0300

    After adding fixtures for goods

--- goods/admin.py
+++ goods/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 
-from goods.models import Categories
+from goods.models import Categories, Products
 
-admin.site.register(Categories)
+
+@admin.register(Categories)
+class CategoriesAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+@admin.register(Products)
+class ProductsAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
--- goods/models.py
+++ goods/models.py
@@ -1,3 +1,4 @@
+from email.policy import default
 from enum import unique
 from tabnanny import verbose
 from django.db import models
@@ -5,11 +6,31 @@ from django.db import models
 
 class Categories(models.Model):
     name = models.CharField(max_length=150, unique=True, verbose_name="Category")
-    slug = models.SlugField(
-        max_length=200, unique=True, blank=True, null=True, verbose_name="URL"
-    )
+    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name="URL")
 
     class Meta:
+        db_table = "Category"
         verbose_name = "Category"
         verbose_name_plural = "Categories"
-        db_table = "Category"
+
+    def __str__(self):
+        return self.name
+
+
+class Products(models.Model):
+    name = models.CharField(max_length=150, unique=True, verbose_name="Product")
+    slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name="URL")
+    description = models.TextField(blank=True, null=True, verbose_name="Description")
+    image = models.ImageField(upload_to='goods_images', blank=True, null=True, verbose_name="Image")
+    price = models.DecimalField(default=0.00, max_digits=7, decimal_places=2, verbose_name="Price")
+    discount = models.DecimalField(default=0.00, max_digits=4, decimal_places=2, verbose_name="Discount in %")
+    quantity = models.PositiveIntegerField(default=0, verbose_name="Quantity")
+    category = models.ForeignKey(to=Categories, on_delete=models.CASCADE, verbose_name="Category")
+
+    class Meta:
+        db_table = "Product"
+        verbose_name = "Product"
+        verbose_name_plural = "Products"
+        
+    def __str__(self):
+        return f'{self.name} Количество - {self.quantity}'
