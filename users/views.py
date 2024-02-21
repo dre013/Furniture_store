97bd31b3e4c7123eb92495f3c071400986b039b2
Andrey Kuznetsov <nice.andrey013@gmail.com>
2024-02-22 02:02:59 +0300

    Profile form, controller & notification are ready

--- users/views.py
+++ users/views.py
@@ -1,12 +1,12 @@
-from django.contrib import auth
+from django.contrib.auth.decorators import login_required
+from django.contrib import auth, messages
 from django.http import HttpResponseRedirect
 from django.shortcuts import redirect, render
 from django.urls import reverse
 
-from users.forms import UserLoginForm, UserRegistrationForm
+from users.forms import UserLoginForm, UserProfileForm, UserRegistrationForm
 
 def login(request):
-
     if request.method == 'POST':
         form = UserLoginForm(data=request.POST)
         if form.is_valid():
@@ -15,6 +15,7 @@ def login(request):
             user = auth.authenticate(username=username, password=password)
             if user:
                 auth.login(request, user)
+                messages.success(request, f'Welcome {username}! You are logged in!')
                 return HttpResponseRedirect(reverse('main:index'))
     else:
         form = UserLoginForm()
@@ -27,30 +28,42 @@ def login(request):
 
 
 def registration(request):
-
     if request.method == 'POST':
         form = UserRegistrationForm(data=request.POST)
         if form.is_valid():
             form.save()
             user = form.instance
             auth.login(request, user)
+            messages.success(request, f'Congratulations {user.username}! You have successfully registered!')
             return HttpResponseRedirect(reverse('main:index'))
     else:
         form = UserRegistrationForm()
+
     context = {
         'title': 'Home - Регистрация',
         'form': form
     }
     return render(request, 'users/registration.html', context)
 
-
+@login_required
 def profile(request):
+    if request.method == 'POST':
+        form = UserProfileForm(data=request.POST, instance=request.user, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your profile is updated successfully')
+            return HttpResponseRedirect(reverse('user:profile'))
+    else:
+        form = UserProfileForm(instance=request.user)
+
     context = {
         'title': 'Home - Кабинет',
+        'form': form
     }
     return render(request, 'users/profile.html', context)
 
-
+@login_required
 def logout(request):
+    messages.success(request, f'{request.user.username}, you are logged out!')
     auth.logout(request)
     return redirect('main:index')
